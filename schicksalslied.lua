e2a08b2f60f93703c984565b80618cdfa3f04b55
William Hazard <105560469+williamthazard@users.noreply.github.com>
2023-05-10 16:05:49 -0400

    Update schicksalslied.lua
    update version number to reflect previous changes (forgot to do this when I pushed previous changes)

--- schicksalslied.lua
+++ schicksalslied.lua
@@ -11,7 +11,7 @@
 ---
 ---use grid to recall history
 ---
----version 1.0.8
+---version 1.0.9
 
 local extensions = '/home/we/.local/share/SuperCollider/Extensions'
 engine.name = util.file_exists(extensions .. '/FormantTriPTR/FormantTriPTR.sc') and 'LiedMotor' or nil
